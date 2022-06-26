2ea6fed3e67d5e469d8f1048db2585f1683976b6
gabe.vela <77593670+gabrielgomezvela@users.noreply.github.com>
2022-06-26 18:17:53 -0400

    labs unit #3

--- survivalgardencollector/main_app/views.py
+++ survivalgardencollector/main_app/views.py
@@ -3,8 +3,17 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # fake data just for now 
+# Add the SurvivalGarden class & list and view function below the imports
+class SurvivalGarden:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, description, age):
+    self.description = description
+    self.age = age
 
-
+survivalgardens = [
+  SurvivalGarden('Tropical', 'Garden for tropical climates.', 5),
+  SurvivalGarden('Desert', 'Garden for dry climates.', 6),
+  SurvivalGarden('Deciduous', 'Garden for Southern Ontario.', 9)
+]
 
 def about(request):
   return render(request, 'about.html')
